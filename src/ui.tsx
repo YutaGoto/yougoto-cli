0507b327c51cda4c9b40b0fc90a3f1706c683e23
YutaGoto <you.goto.510@gmail.com>
2021-04-21 00:23:39 +0900

    update ui

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,10 +1,8 @@
 import React, {FC, useState} from 'react'
-import {Text, useApp} from 'ink'
+import {Box, Text, useApp} from 'ink'
 import Link from 'ink-link'
 import SelectInput from 'ink-select-input'
 import open from 'open'
-
-import Info from './info'
 
 interface SelectItem {
   label: string;
@@ -45,7 +43,25 @@ const App: FC = () => {
         exit()
         break
       case 'info':
-        setInfo(<Info />)
+        setInfo(
+          <Box>
+            <Box borderStyle="round" flexDirection="column" width={50}>
+              <Text>.ごっち</Text>
+              <Text>* エンジニア</Text>
+              <Text>  * パイソン</Text>
+              <Text>  * ジャバスクリプト</Text>
+              <Text>  * ルビィ</Text>
+              <Text>* 活動</Text>
+              <Text>  * IM@Study</Text>
+              <Text>    * アイマス好きが集まるもくもく会運営</Text>
+              <Text>* 趣味</Text>
+              <Text>  * 競技プログラミング(AtCoder: <Text color="#80604D">茶色</Text>)</Text>
+              <Text>  * ゲーム</Text>
+              <Text>    * どうぶつの森</Text>
+              <Text>    * Splatoon2</Text>
+            </Box>
+          </Box>
+        )
         break
       case 'Twitter':
         /* eslint @typescript-eslint/no-floating-promises: 0 */
